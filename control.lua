8d63a0eeae39ce9423fdab7044e573cd3161e32d
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2018-10-11 13:21:46 +0100

    On new/load map hidden lights are updated
    when a new map is generated or a map is loaded all eletric poles are reviewed for compliance with current lights. the lights are then added/removed as required.

--- control.lua
+++ control.lua
@@ -1,9 +1,18 @@
+if ModSettings == nil then
+	ModSettings = {}
+end
+
+
+
 StartUp = function()
-	settings.powerPoleWireReachLightedMultiplier = tonumber(settings.startup["power-pole-wire-reach-lighted-percent"].value) / 100
+	ModSettings.PowerPoleWireReachLightedMultiplier = tonumber(settings.startup["power-pole-wire-reach-lighted-percent"].value) / 100
 end
 
 OnPowerPoleBuilt = function(entity)
 	local poleLightName = entity.name .. "-light"
+	if game.entity_prototypes[poleLightName] == nil then return end
+	local poleLight = entity.surface.find_entity(poleLightName, entity.position)
+	if poleLight ~= nil then return end
 	entity.surface.create_entity{
 		name = poleLightName, 
 		position = entity.position,
@@ -19,20 +28,41 @@ OnPowerPoleRemoved = function(entity)
 	poleLight.destroy()
 end
 
+UpdateHiddenLightEntities = function()
+	if ModSettings.PowerPoleWireReachLightedMultiplier > 0 then
+		for _, surface in pairs(game.surfaces) do
+			for _, pole in pairs(surface.find_entities_filtered{type="electric-pole"}) do
+				OnPowerPoleBuilt(pole)
+			end
+		end
+	else
+		for _, surface in pairs(game.surfaces) do
+			for _, pole in pairs(surface.find_entities_filtered{type="electric-pole"}) do
+				OnPowerPoleRemoved(pole)
+			end
+		end
+	end
+end
+
 
 
 OnBuiltEntity = function(event)
     local entity = event.created_entity
-    if entity.type == "electric-pole" and settings.powerPoleWireReachLightedMultiplier > 0 then
+    if entity.type == "electric-pole" and ModSettings.PowerPoleWireReachLightedMultiplier > 0 then
         OnPowerPoleBuilt(entity)
     end
 end
 
 OnRemovedEntity = function(event)
     local entity = event.entity
-    if entity.type == "electric-pole" and settings.powerPoleWireReachLightedMultiplier > 0 then
+    if entity.type == "electric-pole" and ModSettings.PowerPoleWireReachLightedMultiplier > 0 then
 		OnPowerPoleRemoved(entity)
 	end
+end
+
+OnFirstTick = function()
+	script.on_event(defines.events.on_tick, nil)
+	UpdateHiddenLightEntities()
 end
 
 
@@ -48,3 +78,4 @@ script.on_event(defines.events.on_robot_built_entity, OnBuiltEntity)
 script.on_event(defines.events.on_player_mined_entity, OnRemovedEntity)
 script.on_event(defines.events.on_entity_died, OnRemovedEntity)
 script.on_event(defines.events.on_robot_mined_entity, OnRemovedEntity)
+script.on_event(defines.events.on_tick, OnFirstTick)
