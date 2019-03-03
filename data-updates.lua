47613d2d5b43c76ece846450f0482cb4e64d166b
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2019-03-03 17:44:26 +0000

    fixed old code to be compatible with 0.17

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,10 @@
 --[[
 	Done as a data-update to allow other mods to manipulate the base lamp before we clone it. ie: change turn on/off times.
 ]]
+
+local Constants = require("constants")
+
+
 GenerateHiddenLights = function()
 	local poleLights = {}
 	for tile=1,75 do
@@ -19,17 +23,24 @@ GenerateHiddenLight = function(tile, name)
 	hiddenLight.selection_box = nil --makes a nice cross on the powered area rather than a default sized box
 	hiddenLight.selectable_in_game = false
 	hiddenLight.picture_off = {
-		filename = "__base__/graphics/entity/small-lamp/lamp.png",
-		width = 0,
-		height = 0
+		filename = Constants.AssetModName .. "/graphics/transparent.png",
+		priority = "very-low",
+		width = 1,
+		height = 1
 	}
 	hiddenLight.picture_on = {
-		filename = "__base__/graphics/entity/small-lamp/lamp.png",
-		width = 0,
-		height = 0
+		filename = Constants.AssetModName .. "/graphics/transparent.png",
+		priority = "very-low",
+		width = 1,
+		height = 1
 	}
 	hiddenLight.light = {intensity = 0.6, size = lightRange, color = {r=1.0, g=1.0, b=1.0}}
-	hiddenLight.energy_usage_per_tick = settings.startup["light-power-usage-watts"].value .. "W"
+	if settings.startup["light-power-usage-watts"].value > 0 then
+		hiddenLight.energy_usage_per_tick = settings.startup["light-power-usage-watts"].value .. "W"
+	else
+		hiddenLight.energy_source.type = "void"
+		hiddenLight.energy_usage_per_tick = "1W"
+	end
 	hiddenLight.energy_source.render_no_network_icon = false
 	hiddenLight.energy_source.render_no_power_icon = false
 	return hiddenLight
@@ -45,12 +56,11 @@ GenerateHiddenLightEletricPole = function()
 	hiddenLightPole.maximum_wire_distance = 0
 	hiddenLightPole.supply_area_distance = 0.1
 	hiddenLightPole.pictures = {
-		filename = "__base__/graphics/entity/small-electric-pole/small-electric-pole.png",
-		priority = "extra-high",
+		filename = Constants.AssetModName .. "/graphics/transparent.png",
+		priority = "very-low",
 		width = 1,
-		height = 0,
-		direction_count = 4,
-		shift = {1.4, -1.1}
+		height = 1,
+		direction_count = 4
 	}
 	data:extend({hiddenLightPole})
 end
